8c3d40cb352ecfbb9f4b4112bc0bd230f6c3dff4
KyunHwan <kyunhwan.peter@gmail.com>
2021-03-01 16:56:41 -0500

    timer

--- app/src/main/java/com/example/peopleprotector/MainActivity.kt
+++ app/src/main/java/com/example/peopleprotector/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.peopleprotector
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val timerStart: Button = findViewById(R.id.timerTrigger)
+
+
+        timerStart.setOnClickListener{
+            move2Timer()
+        }
     }
+
+    private fun move2Timer(){
+        val intent: Intent = Intent(this, Timer::class.java)
+        startActivity(intent)
+    }
+
 }
