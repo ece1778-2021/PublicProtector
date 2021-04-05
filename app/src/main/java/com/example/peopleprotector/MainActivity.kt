af929d5c6049a9c88ad775ee8701cf6dd1ce1da0
MertTens <amertens114@gmail.com>
2021-04-04 22:50:24 -0400

    asdf

--- app/src/main/java/com/example/peopleprotector/MainActivity.kt
+++ app/src/main/java/com/example/peopleprotector/MainActivity.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
